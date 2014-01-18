7d543580c391a9f7330fca6d3ebc57a767fccf5b
Bob Clough <bob@clough.me>
2014-01-18 17:18:31 +0000

    added pcb-gcode settings

---
+++ pcb-machine.h
@@ -0,0 +1,24 @@
+//
+// For ease of use, and to avoid overwritting your settings,
+// use pcb-gcode-setup to make changes to these settings.
+//
+
+real DEFAULT_Z_HIGH = 10.000000;
+real DEFAULT_Z_UP = 0.300000;
+real DEFAULT_Z_DOWN = -0.100000;
+real DRILL_DEPTH = -2.000000;
+real DRILL_DWELL = 1.000000;
+real SPINDLE_ON_TIME = 3.000000;
+real MILLING_DEPTH = -2.000000;
+real TEXT_DEPTH = -0.127000;
+real TOOL_CHANGE_POS_X = 0.000000;
+real TOOL_CHANGE_POS_Y = 0.000000;
+real TOOL_CHANGE_POS_Z = 3.000000;
+real FEED_RATE = 150.000000;
+real FEED_RATE_Z = 50.000000;
+real DEFAULT_WIDTH = 0.400000;
+real X_OFFSET = 0.000000;
+real Y_OFFSET = 0.000000;
+real X_HOME = 0.000000;
+real Y_HOME = 0.000000;
+real EPSILON = 0.100000;
